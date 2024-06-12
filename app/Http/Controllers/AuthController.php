b9ed669092adfd721a35916f2546fdae5292b90c
vlcp197 <vlcp197@gmail.com>
2024-06-11 21:30:01 -0300

    Error treatment applied for each field in the form; Controller rules applied for each field in the form

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     public function register(Request $request){
-        dd($request);
+        $request->validate([
+            'username' => ['required', 'max:255'],
+            'email' => ['required', 'max:255', 'email'],
+            'password' => ['required','min:3', 'confirmed']
+        ]);
+        dd("ok");
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,25 +7,35 @@
             {{-- username --}}
             <div class="mb-4">
                 <label for="username">Username</label>
-                <input type="text" name="username" class="input">
+                <input type="text" name="username" class="input @error('username') ring-red-500 @enderror" value="{{ old('username')}}">
+                @error('username')
+                    <p class="error">{{ $message }}</p>
+                @enderror
             </div>
             
             {{-- Email --}}
             <div class="mb-4">
                 <label for="email">Email</label>
-                <input type="text" name="email" class="input">
+                <input type="text" name="email" class="input @error('email') ring-red-500 @enderror" value="{{ old('email')}}">
+                @error('email')
+                    <p class="error">{{$message}}</p>
+                @enderror
             </div>
             
             {{-- Password --}}
             <div class="mb-4">
                 <label for="password">Password</label>
-                <input type="password" name="password" class="input">
+                <input type="password" name="password" class="input @error('password') ring-red-500 @enderror">
+                @error('password')
+                    <p class="error">{{$message}}</p>
+                @enderror
             </div>
             
             {{-- Confirm Password --}}
             <div class="mb-4">
                 <label for="password_confirmation">Confirm Password</label>
-                <input type="password" name="password_confirmation" class="input">
+                <input type="password" name="password_confirmation" class="input @error('password') ring-red-500 @enderror">
+                
             </div>
             
             {{-- Submit Button --}}
