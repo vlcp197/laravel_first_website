0767d04d23de41a5d713c8d78f7f5a00ea58fb24
vlcp197 <vlcp197@gmail.com>
2024-06-12 18:23:30 -0300

    Validate email and redirect user to index page

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,16 +2,25 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class AuthController extends Controller
 {
     public function register(Request $request){
-        $request->validate([
+        $fields = $request->validate([
             'username' => ['required', 'max:255'],
-            'email' => ['required', 'max:255', 'email'],
+            'email' => ['required', 'max:255', 'email', 'unique:users'],
             'password' => ['required','min:3', 'confirmed']
         ]);
-        dd("ok");
+
+        $user = User::create($fields);
+        
+        Auth::login($user);
+
+        return redirect()->route('home');
     }
 }
