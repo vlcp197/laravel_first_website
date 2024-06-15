4c1b168dea975963f4701200756034f54725f570
vlcp197 <vlcp197@gmail.com>
2024-06-15 14:25:43 -0300

    Implement Alpine js

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ env('APP_NAME') }}</title>
     @vite(["resources/css/app.css", "resources/js/app.js"])
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     
 </head>
 
@@ -15,7 +16,19 @@
             <a href="{{ route('home')}}" class="nav-link">Home</a>
 
             @auth
-                    
+                <div class="relative grid place-items-center" x-data="{open: false}">
+                    {{-- Dropdown menu button --}}
+                    <button @click="open = !open" class="round-btn" type="button">
+                        <img src="https://picsum.photos/200" alt="">
+                    </button>
+
+                    {{-- Dropdown menu --}}
+                    <div x-show="open" @click.outside="open=false" class="bg-white shadow-lg absolute top-10 right-0 rounded-lg overflow-hidden font-light">
+                        <p class="username">{{ auth()->user()->username }}</p>
+                        <a href="" class="block hover:bg-slate-100 pl-4 pr-8 py-2 mb-1">Dashboard</a>
+
+                    </div>
+                </div>
             @endauth
 
             @guest
